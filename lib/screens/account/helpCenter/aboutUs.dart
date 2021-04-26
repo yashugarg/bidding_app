8bd84809c512c2cbe135d4f705c7b0c095101311
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2021-04-26 18:32:56 +0530

    modified the aboutUs screen and added icon.png

--- lib/screens/account/helpCenter/aboutUs.dart
+++ lib/screens/account/helpCenter/aboutUs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutUs extends StatefulWidget {
   @override
@@ -10,7 +11,138 @@ class _AboutUsState extends State<AboutUs> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("About Us"),
+        leading: new IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () => Navigator.of(context).pop()),
+        backgroundColor: Color(0xFF4A3298),
+        title: Text('About Us',
+            style: TextStyle(fontSize: 19, color: Colors.white)),
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: null),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/icon.png'),
+                  radius: 40.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Center(
+                child: Text(
+                  "Bidder Inc.",
+                  style: TextStyle(
+                    fontSize: 26.0,
+                    color: Colors.blueGrey[800],
+                  ),
+                ),
+              ),
+              Divider(
+                height: 60.0,
+                color: Colors.grey,
+              ),
+              Text('Artists and Programmers:',
+                  style: TextStyle(
+                    color: Colors.blueGrey[800],
+                    letterSpacing: 2.0,
+                    fontSize: 16.0,
+                  )),
+              SizedBox(height: 15.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.blueGrey[700],
+                  ),
+                  SizedBox(width: 15.0),
+                  Text(
+                    'Akshit Jain',
+                    style: TextStyle(
+                      color: Colors.blueGrey[900],
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.blueGrey[700],
+                  ),
+                  SizedBox(width: 15.0),
+                  Text(
+                    'Bhavya Tewari',
+                    style: TextStyle(
+                      color: Colors.blueGrey[900],
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.blueGrey[700],
+                  ),
+                  SizedBox(width: 15.0),
+                  Text(
+                    'Bhavya Verma',
+                    style: TextStyle(
+                      color: Colors.blueGrey[900],
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.blueGrey[700],
+                  ),
+                  SizedBox(width: 15.0),
+                  Text(
+                    'Gaurav Dubey',
+                    style: TextStyle(
+                      color: Colors.blueGrey[900],
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.blueGrey[700],
+                  ),
+                  SizedBox(width: 15.0),
+                  Text(
+                    'Yashu Garg',
+                    style: TextStyle(
+                      color: Colors.blueGrey[900],
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ],
+              ),
+            ]),
       ),
     );
   }
